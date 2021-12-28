971a3248546b9b12235f7e57d9ef765e84754f98
aericknunez <aerick.nunez@gmail.com>
2021-12-27 22:08:30 -0600

    Ignore

--- resources/views/components/comandero/iconos/categorias.blade.php
+++ resources/views/components/comandero/iconos/categorias.blade.php
@@ -6,6 +6,11 @@
                             <img src="{{ asset("img/ico/default.png") }}" width="80" height="80" class="img-fluid mx-auto mb-1 rounded-pill">
                             <p class="mb-1 text-dark">Principal</p>
                             </div>
+                        </a><a  wire:click="btnCatSelect(2)" wire:loading.class="disabled" wire:target="btnCatSelect(2)">
+                            <div class="near_item bg-white box_rounded mr-2 p-3 text-center my-1 shadow-sm pointer">
+                            <img src="{{ asset("img/ico/0dbcffe255.png") }}" width="80" height="80" class="img-fluid mx-auto mb-1 rounded-pill">
+                            <p class="mb-1 text-dark">Combos</p>
+                            </div>
                         </a></div>
         </section>
 
--- resources/views/components/venta/lateral-modal-tpago.blade.php
+++ resources/views/components/venta/lateral-modal-tpago.blade.php
@@ -9,6 +9,7 @@
     
               <div class="text-center"><a class="btn {{ getColorBoton(1) }}" wire:click="btnTipoPago(1)" wire:key="1"><i class="fas fa-dollar-sign mr-1"></i>Efectivo</a>
                 <a class="btn {{ getColorBoton(2) }}" wire:click="btnTipoPago(2)" wire:key="2"><i class="fas fa-credit-card mr-1"></i>Tarjeta de Credito</a>
+                <a class="btn {{ getColorBoton(3) }}" wire:click="btnTipoPago(3)" wire:key="3"><i class="fab fa-bitcoin mr-1"></i>BitCoin</a>
                 
         
                 </div>          
