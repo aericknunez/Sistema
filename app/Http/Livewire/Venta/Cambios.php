34c0e7cfe9cef2c6a3a2fa488397038012cdad70
aericknunez <aerick.nunez@gmail.com>
2021-12-28 04:35:54 -0600

    Imprime

--- app/Http/Livewire/Venta/Cambios.php
+++ app/Http/Livewire/Venta/Cambios.php
@@ -253,7 +253,7 @@ class Cambios extends Component
 
 
     public function btnImprimirPreCuenta(){ /// Imprime la precuenta
-        $this->ImprimirPrecuenta($this->clientSelected);
+        $this->ImprimirPrecuenta($this->clientSelected, $this->propinaCantidad, $this->propinaPorcentaje);
         $this->dispatchBrowserEvent('realizado', ['clase' => 'success', 'titulo' => 'Imprimiendo', 'texto' => 'Imprimiendo Pre Cuenta']);
     
     } 
