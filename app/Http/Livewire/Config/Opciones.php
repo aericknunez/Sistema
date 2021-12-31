c571cc59be918a68aa75a2291c85c1603d57d03a
aericknunez <aerick.nunez@gmail.com>
2021-12-30 18:47:39 -0600

    con

--- app/Http/Livewire/Config/Opciones.php
+++ app/Http/Livewire/Config/Opciones.php
@@ -75,23 +75,23 @@ class Opciones extends Component
 
 
     public function cambioDatabase(){
-        $tablas = SyncTable::all();
+        // $tablas = SyncTable::all();
         $counter = 0;
 
-        foreach ($tablas as $table) {
-            if ($table) {
-                if (DB::table($table)
-                ->where('td', 0)
-                ->update(['td' => session('sistema.td')])) {
-                   $counter ++;
-                }
-            }
+        // foreach ($tablas as $table) {
+        //     if ($table) {
+        //         if (DB::table($table)
+        //         ->where('td', 0)
+        //         ->update(['td' => session('sistema.td')])) {
+        //            $counter ++;
+        //         }
+        //     }
 
-        }
+        // }
 
-        // OrderImg::where('td', 0)->update(['td' => session('sistema.td')]);
-        // Producto::where('td', 0)->update(['td' => session('sistema.td')]);
-        // ProductoCategoria::where('td', 0)->update(['td' => session('sistema.td')]);
+        OrderImg::where('td', 0)->update(['td' => session('sistema.td')]);
+        Producto::where('td', 0)->update(['td' => session('sistema.td')]);
+        ProductoCategoria::where('td', 0)->update(['td' => session('sistema.td')]);
 
 
         $this->dispatchBrowserEvent('mensaje', 
