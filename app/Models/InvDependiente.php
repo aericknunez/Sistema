6531cc2561f3e20b24c09f2abb6f81bb4d8e3743
aericknunez <aerick.nunez@gmail.com>
2022-02-05 04:55:59 -0600

    Migraciones

---
+++ app/Models/InvDependiente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvDependiente extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_02_04_065635_create_inventarios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('producto');
+            $table->double('cantidad', 10, 4);
+            $table->double('minimo', 10, 4);
+            $table->integer('mostrar')->nullable(true);
+            $table->string('unidad', 200)->nullable(true);
+
+            $table->string('clave', 25)->nullable(true);
+            $table->string('tiempo', 25)->nullable(true);
+            $table->integer('td')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventarios');
+    }
+}
