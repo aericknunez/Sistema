c6a9cab1fc5561a0b951201c39f537767828570e
aericknunez <aerick.nunez@gmail.com>
2022-02-05 05:57:02 -0600

    Update Historial.php

--- app/System/Historial/Historial.php
+++ app/System/Historial/Historial.php
@@ -115,7 +115,7 @@ trait Historial {
 
 
     public function ordenesUnica($fecha){
-        return TicketOrden::whereDay('created_at','=', '2022-02-04')->get();
+        return TicketOrden::whereDay('created_at','=', '2022-02-04 00:00:00')->get();
     }
 
     public function ordenesMultiple($fecha1, $fecha2){
