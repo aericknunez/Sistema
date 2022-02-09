666a046e19b62ee9d925b8aa0de3dd2ec988798e
aericknunez <aerick.nunez@gmail.com>
2022-02-09 15:08:42 -0600

    Descontar

--- app/System/Inventario/Administrar.php
+++ app/System/Inventario/Administrar.php
@@ -1,6 +1,8 @@
 <?php
-namespace App\System\Historial;
+namespace App\System\Inventario;
 
+use App\Models\Producto;
+use App\Models\TicketProducto;
 
 /*
     Administra el inventario, este Trait se encarga de descontar los productos del inventario adecuadamente
@@ -9,6 +11,21 @@ namespace App\System\Historial;
 trait Administrar {
 
 
+    public function descontarProductosInventario($factura, $tipo){
+        $productos = $this->buscarProductosFactura($factura, $tipo);
+        // dd($productos);
+        dump($productos);
+    }
+
+
+    private function buscarProductosFactura($factura, $tipo){
+        return TicketProducto::select('cod')->addSelect(['iden' => Producto::select('id')
+                                ->whereColumn('ticket_productos.cod', 'productos.cod')])
+                                ->where('num_fact', $factura)
+                                ->where('tipo_venta', $tipo)
+                                ->where('edo', 1)
+                                ->get();
+    }
 
     
 
