ae6c259c96208cbb1700f3d042810398c9520013
aericknunez <aerick.nunez@gmail.com>
2022-02-15 13:45:37 -0600

    Resusmen

--- resources/views/historial/resumen.blade.php
+++ resources/views/historial/resumen.blade.php
@@ -3,32 +3,5 @@
     {{-- Contenido --}}
         @livewire('historial.resumen')
     {{-- contenido  --}}
-   
-    {{-- @push('scripts')
-    <script>
-
-        window.addEventListener('graficar', event => {
-
-                var ctxP = document.getElementById("pieChart").getContext('2d');
-                var myPieChart = new Chart(ctxP, {
-                    type: 'pie',
-                    data: {
-                        labels: ["Facturado", "No Facturado"],
-                        datasets: [{
-                            data: [event.detail.facturado, event.detail.nofacturado],
-                            backgroundColor: ["#46BFBD", "#F7464A"],
-                            hoverBackgroundColor: ["#5AD3D1","#FF5A5E"]
-                        }]
-                    },
-                    options: {
-                        responsive: true
-                    }
-                });
-
-        });
-
-    </script>
-    @endpush --}}
-
 
 </x-principal-layout>
