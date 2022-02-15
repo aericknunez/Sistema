0bdfc574f1a477751ef239c5f2b7ada1fc19bcb1
aericknunez <aerick.nunez@gmail.com>
2022-02-15 05:59:00 -0600

    Fecha

--- resources/views/livewire/historial/reporte.blade.php
+++ resources/views/livewire/historial/reporte.blade.php
@@ -60,7 +60,7 @@
                     <div class="col mx-2">
 
                            <div class="md-form md-outline input-with-post-icon datepicker">
-                            <input wire:model.defer="fecha1" type="date" id="fecha1" name="fecha1" class="form-control">
+                            <input wire:model.defer="fecha1f" type="date" id="fecha1" name="fecha1" class="form-control">
                             <label for="example">Seleccione una fecha</label>
                           </div>
 
