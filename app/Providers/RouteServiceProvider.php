8adc19d54dcf21ce9c9f22b3bf82c29059cc7d93
aericknunez <aerick.nunez@gmail.com>
2022-03-11 06:05:45 -0600

    Revert "Config Tenant"
    This reverts commit a06175c895f92186a6733251eff50d1b907f6be4.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,21 +38,12 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-
-            $api = ['api'];
-            $web = ['web'];
-
-            if (config('multitenancy.enabled')) {
-                array_push($api, 'tenant.api');
-                array_push($web, 'tenant.web');
-            }
-
             Route::prefix('api')
-                ->middleware($api)
+                ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware($web)
+            Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
