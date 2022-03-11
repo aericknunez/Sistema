4e34a700ef3f462372a7c15e811cb73dbaaa2485
aericknunez <aerick.nunez@gmail.com>
2022-03-11 07:20:33 -0600

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware(['guest'])->get('/', function () {
 
     $priv = ConfigPrivate::first();
-    if ($priv->sys_login) {
-        return view('login');
+    if ($priv) {
+        if ($priv->sys_login) {
+            return view('login');
+        } else {
+            return view('auth.login');
+        }
     } else {
         return view('auth.login');
     }
