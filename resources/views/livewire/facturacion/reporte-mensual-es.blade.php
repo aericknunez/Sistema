65520e866171b6de9da0af0b52568d86a47a083b
aericknunez <aerick.nunez@gmail.com>
2023-02-22 14:59:51 -0600

    Link Imprimir

--- resources/views/livewire/facturacion/reporte-mensual-es.blade.php
+++ resources/views/livewire/facturacion/reporte-mensual-es.blade.php
@@ -102,7 +102,9 @@
                 </div>
 
                     <div class="text-right">
-                        <a href="{{ route('reportePDF', ['busqueda' => $busqueda, 'anio' => $anio, 'mes' => $mes]) }}" class="btn btn-link btn-sm"  ><i class="fas fa-print mr-1"></i> Imprimir</a>
+                        @if ($datos)
+                        <a href="{{ route('reportePDF', ['busqueda' => $busqueda, 'anio' => $anio, 'mes' => $mes]) }}" class="btn btn-link btn-sm"  ><i class="fas fa-print mr-1"></i> Imprimir</a>  
+                        @endif
 
                         <button class="btn btn-mdb-color" type="submit"><i class="fas fa-save mr-1"></i> Guardar</button>
                     </div>
