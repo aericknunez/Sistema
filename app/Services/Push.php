e7b9caa55a32867167d101f3313a432721aefc67
Erick Nuñez <aerick.nunez@gmail.com>
2024-07-30 16:16:02 -0600

    Update Push.php

--- app/Services/Push.php
+++ app/Services/Push.php
@@ -173,7 +173,7 @@ class Push {
                             ])->post(env('URL_SYNC'), [
                                 'file' => json_encode($archivo)
                             ]);
-
+              return $response;              
             if ($response->successful()) {
                 Log::info('Sync process completed successfully.');
                 SyncUp::where('comprobacion', Session::get('hash_name'))->update(['edo' => 3]);
