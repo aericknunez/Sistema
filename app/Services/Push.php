b17f244cbf5db2dbbfd583348f4e45fefb13aadd
Erick Nuñez <aerick.nunez@gmail.com>
2024-07-30 16:14:36 -0600

    Update Push.php

--- app/Services/Push.php
+++ app/Services/Push.php
@@ -180,7 +180,7 @@ class Push {
                 return $response["message"];
             } else {
                 Log::error('Sync process failed.', ['response' => $response->body()]);
-                return $response->body();
+                return "No se ejecuto la accion";
             }
         } catch (\Exception $e) {
             Log::error('Sync process failed.', ['message' => $e->getMessage()]);
